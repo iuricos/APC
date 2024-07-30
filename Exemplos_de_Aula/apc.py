da8c0538d990d1f3f89fffd2a4b1ab71604c9199
iuricos <iuri.ccavalcante@gmail.com>
2024-07-29 23:44:32 -0300

    exemplos da aula 30/07

---
+++ Exemplos_de_Aula/apc.py
@@ -0,0 +1,26 @@
+"""x = int(input())
+y = 1
+z = 0
+
+for y in range(1, x):
+    if (x % y == 0):
+        print(y, end=' ')
+        z += y
+    
+if x == z:
+    print("Perfeito")
+else:
+    print("n eh")
+
+# beecrowd
+"""
+
+x = int(input())
+
+a, b = 0, 1
+
+print(f )
+for i in range(x-2):
+    print(b, end=', ')
+    a, b = b, a+b
+    
