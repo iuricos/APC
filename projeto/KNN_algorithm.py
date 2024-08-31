dadc4ed80c932e34729523a63c683856f111639c
iuricos <iuri.ccavalcante@gmail.com>
2024-08-31 00:37:55 -0300

    recomeçadno

--- projeto/KNN_algorithm.py
+++ projeto/KNN_algorithm.py
@@ -1,9 +1,3 @@
-K = int(input())
-Ntrain, Ntest = map(int, input().split())
-
-Xtrain = []
-Xtrain_num = []
-#caraio
 cap_shape = ['b', 'c', 'x', 'f', 'k', 's']
 cap_surface = ['f', 'g', 'y', 's']
 cap_color = ['n', 'b', 'c', 'g', 'r', 'p', 'u', 'e', 'w', 'y']
@@ -15,10 +9,10 @@ gill_size = ['b', 'n']
 gill_color = ['k', 'n', 'b', 'h', 'g', 'r', 'o', 'p', 'u', 'e', 'w', 'y']
 stalk_shape = ['e', 't']
 stalk_root = ['b', 'c', 'u', 'e', 'z', 'r', '?']
-stalk_surface_above = ['f', 'y', 'k', 's']
-stalk_surface_below = ['f', 'y', 'k', 's']
-stalk_color_above = ['n', 'b', 'c', 'g', 'o', 'p', 'e', 'w', 'y']
-stalk_color_below = ['n', 'b', 'c', 'g', 'o', 'p', 'e', 'w', 'y']
+stalk_surface_above_ring = ['f', 'y', 'k', 's']
+stalk_surface_below_ring = ['f', 'y', 'k', 's']
+stalk_color_above_ring = ['n', 'b', 'c', 'g', 'o', 'p', 'e', 'w', 'y']
+stalk_color_below_ring = ['n', 'b', 'c', 'g', 'o', 'p', 'e', 'w', 'y']
 veil_type = ['p', 'u']
 veil_color = ['n', 'o', 'w', 'y']
 ring_number = ['n', 'o', 't']
@@ -27,35 +21,46 @@ spore_print_color = ['k', 'n', 'b', 'h', 'r', 'o', 'u', 'w', 'y']
 population = ['a', 'c', 'n', 's', 'v', 'y']
 habitat = ['g', 'l', 'm', 'p', 'u', 'w', 'd']
 
-for i in range(Ntrain):
-    cogu = input().split()
-    Xtrain.append(cogu)
-    
-for i in Xtrain:
-    atr = [
-            cap_shape.index(i[0]),
-            cap_surface.index(i[1]),
-            cap_color.index(i[2]),
-            bruises.index(i[3]),
-            odor.index(i[4]),
-            gill_attachment.index(i[5]),
-            gill_spacing.index(i[6]),
-            gill_size.index(i[7]),
-            gill_color.index(i[8]),
-            stalk_shape.index(i[9]),
-            stalk_root.index(i[10]),
-            stalk_surface_above.index(i[11]),
-            stalk_surface_below.index(i[12]),
-            stalk_color_above.index(i[13]),
-            stalk_color_below.index(i[14]),
-            veil_type.index(i[15]),
-            veil_color.index(i[16]),
-            ring_number.index(i[17]),
-            ring_type.index(i[18]),
-            spore_print_color.index(i[19]),
-            population.index(i[20]),
-            habitat.index(i[21])
-        ]
-    Xtrain_num.append(atr)
+nomes_atr = [cap_shape, cap_surface, cap_color, bruises, odor, gill_attachment, gill_spacing, gill_size, gill_color, stalk_shape, stalk_root, stalk_surface_above_ring, stalk_surface_below_ring, stalk_color_above_ring, stalk_color_below_ring, veil_type, veil_color, ring_number, ring_type, spore_print_color, population, habitat]
 
-print(Xtrain_num)
+# f s g t f f c b w t b f s w w p w o p h v g
+
+K =  int(input())
+Ntrain, Ntest = map(int, input().split())
+
+Xtrain = []
+Xtrain_num = []
+cogu_numerado = []
+atributos_numerados = []
+bagulho = int()
+
+
+for i in range(Ntrain):
+    car_cogu = input().split()
+    Xtrain.append(car_cogu)
+
+for i in Xtrain:
+    cogu_numerado = [cap_shape.index(i[0]), cap_surface.index(i[1]), cap_color.index(i[2]), bruises.index(i[3]), odor.index(i[4]), gill_attachment.index(i[5]), gill_spacing.index(i[6]), gill_size.index(i[7]), gill_color.index(i[8]), stalk_shape.index(i[9]), stalk_root.index(i[10]), stalk_surface_above_ring.index(i[11]), stalk_surface_below_ring.index(i[12]), stalk_color_above_ring.index(i[13]), stalk_color_below_ring.index(i[14]), veil_type.index(i[15]), veil_color.index(i[16]), ring_number.index(i[17]), ring_type.index(i[18]), spore_print_color.index(i[19]), population.index(i[20]), habitat.index(i[21])]
+    Xtrain_num.append(cogu_numerado)
+
+for tipo_atributo in nomes_atr:
+    tipo_atributo_num = list(zip(tipo_atributo, range(len(tipo_atributo))))
+    mapeada = []
+    for _, n in tipo_atributo_num:
+        mapeada.append(n)
+    atributos_numerados.append(mapeada)
+
+
+medias = []
+
+for atributo in atributos_numerados:
+    media = sum(atributo)/len(atributo)
+    medias.append(media)
+    i = 1
+
+    while i != Ntrain:
+        bagulho += (atributo[i]- media[i])**2
+    
+    desvio = (bagulho/Ntrain)**(1/2)
+    print(desvio)
+
